27edddb9a0ba23f48ee96d80bc056e22e0c776c8
Fernando La Chica <flachica@sdi.es>
2026-05-24 08:59:21 +0000

    [flachica] Add migrated addons from moduon/19.0-mig-l10n_es_aeat_sii_oca
    Módulos: l10n_es_aeat_mod111, l10n_es_aeat_mod115, l10n_es_aeat_mod123, l10n_es_aeat_mod130, l10n_es_aeat_mod190, l10n_es_aeat_mod216, l10n_es_aeat_mod296, l10n_es_aeat_mod303, l10n_es_aeat_mod349, l10n_es_aeat_mod390, l10n_es_aeat_sii_oca, l10n_es_partner
    Fuente:  https://github.com/moduon/l10n-spain.git @ 19.0-mig-l10n_es_aeat_sii_oca
    Base:    https://github.com/OCA/l10n-spain.git @ 19.0

--- l10n_es_partner/wizard/l10n_es_partner_wizard.py
+++ l10n_es_partner/wizard/l10n_es_partner_wizard.py
@@ -9,7 +9,6 @@ import os
 import tempfile
 
 from odoo import fields, models, tools
-from odoo.modules.module import get_module_path
 
 from ..gen_src.gen_data_banks import gen_bank_data_csv
 
@@ -73,8 +72,7 @@ class L10nEsPartnerImportWizard(models.TransientModel):
             self.env["res.bank"].load(fieldnames, data)
 
     def import_local(self):
-        module_path = get_module_path("l10n_es_partner")
-        path = os.path.join(module_path, "wizard", "data_banks.csv")
+        path = os.path.join("l10n_es_partner", "wizard", "data_banks.csv")
         if path and os.path.exists(path):
             self._load_csv_data(path)
         else:
